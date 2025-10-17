eebb4269c008e43796a527b0557ff45b6fe8598d
joseleo <joseleogon159@gmail.com>
2025-10-17 16:27:14 -0500

    fix: mostrar código de barras como base64 compatible con Render6

--- wellness/urls.py
+++ wellness/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from django.conf import settings              # 👈 importar settings
+from django.conf import settings
 from django.conf.urls.static import static  
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("select2/", include("django_select2.urls")),
-    path("inventory/", include("inventory.urls")),  # ← ahora sí tendrán prefijo /inventory/
-    # 👇 Redirige automáticamente el dominio raíz a /inventory/
-    path("", RedirectView.as_view(url="/inventory/", permanent=False)),
+    path("inventory/", include("inventory.urls")),  # tu app principal
+
+    # 👇 Redirigir raíz directamente al login
+    path("", RedirectView.as_view(url="/inventory/login/", permanent=False)),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
