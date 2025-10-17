f1da106cdabe2bf39aeb11482f013f02d826f2ed
joseleo <joseleogon159@gmail.com>
2025-10-17 16:26:07 -0500

    fix: mostrar código de barras como base64 compatible con Render5

--- wellness/urls.py
+++ wellness/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("select2/", include("django_select2.urls")),
     path("inventory/", include("inventory.urls")),  # ← ahora sí tendrán prefijo /inventory/
+    # 👇 Redirige automáticamente el dominio raíz a /inventory/
+    path("", RedirectView.as_view(url="/inventory/", permanent=False)),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
